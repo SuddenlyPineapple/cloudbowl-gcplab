1635d8ae2b9f6b5d70f35398727b1bc6f0ccb970
Wojciech Kasperski <wojciech.kasperski@allegro.pl>
2021-12-13 12:07:00 +0100

    Update repo-name

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
+//import java.util.Random;
 
 @SpringBootApplication
 @RestController
